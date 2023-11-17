7d9f4ae8cddd41d2f0561e1e6caf5e3f9058074b
minhtuan236 <150664549+minhtuan236@users.noreply.github.com>
2023-11-17 17:14:38 +0700

    ProductDesgin

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import './App.css';
 import Product from './Product';
 import Review from './Review';
 import ProductList from './ProductList';
-import Footer from './Footer';
 import ProductInfo from './ProductInfo';
 
 function App() {
   return (
     <div className="App">
-      <img src="/image/LOGO.jpg" alt="logo" className='logo'/>
       <Product />
       <ProductInfo />
       <Review />
       <ProductList />
-      <Footer />
     </div>
   );
 }
--- src/Product.jsx
+++ src/Product.jsx
@@ -13,24 +13,30 @@ function Product() {
             <main class="col-md-5">
                 <article>
                     <div class="mb-5 " className='productbox'>
-                        <h1 class="title">Chanel Perfume</h1>
+                        
+                        <h1 className='product-title' >Chanel Perfume</h1>
                         <h4>5.0 <MdOutlineStarPurple500 /><MdOutlineStarPurple500 /><MdOutlineStarPurple500 /><MdOutlineStarPurple500 /><MdOutlineStarPurple500 />| 88 rating | <GoPlusCircle />Compare</h4>
-                        <b className='price'>$1000 </b>
+                        <br />
+                        <b className='price'>$ 1.000 </b>
                             <div className='quantity'>
-                                Quantity
-                                <button type="button"> 
+                                <h4>
+                                    Quantity
+                                </h4>
+                                <div className='abc'>
+                                <button type="button" className='product-minus-button'> 
                                     <svg width="22" height="20" fill="#black" viewBox="0 0 24 24">
                                         <path d="M19 13H5v-2h14v2z"></path>
                                     </svg>
                                 </button>
 
-                                        <input class="text-center" value="14" className='input'/>
-                                                
+                                        <input value="14" className='input'/>       
+                                
                                 <button type="button"> 
                                     <svg  width="22" height="20" fill="#black" viewBox="0 0 24 24">
                                         <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
                                     </svg>
                                 </button>
+                                </div>
                             </div>
 
                             <div className='AddBuy'>
